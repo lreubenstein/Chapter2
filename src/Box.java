8ba2029d1d333c149d55093182af27f4aaecf33d
lreubenstein <lreubenstein@billericak12.com>
2020-10-13 13:32:52 -0400

    Box class

--- src/Box.java
+++ src/Box.java
@@ -1,89 +1,56 @@
-// Joe student
+public class Box {
+    private String type;
+    private int height;
+    private String color;
+    private int width;
+    private int length;
+    private int howManyPencils;
 
-public class Box {		// box class - classes start with upper case
-	private double height;		//attributes
-	private double width;
-	private double length;
-	private int numberOnions;
-	private String color;
-	private Boolean hasALid;
-	
-	public Box(double h, double w, double l, String c, Boolean lid) {		// constructor
-		// TODO Auto-generated constructor stub
-		height = h;
-		width = w;
-		length = l;
-		color = c;
-		hasALid = lid;
-		numberOnions = 0;
-		System.out.println("constructing a box");
-	}
-	public Box() { //default constructor
-		height = 1.0;
-		width = 1.0;
-		length = 1.0;
-		color = "purple";
-		hasALid = false;
-		numberOnions = 0;	
-		System.out.println("default constructor");
-	}
-	
-	public double volume() {		// return type is double
-		double vol = length*width*height;
-		return vol;		// return value
-	}
-	
-	public void addOnions(int num) {
+    public Box(String t, int h, int l, int w, String c){
+        type = t;
+        height = h;
+        length = l;
+        width = w;
+        color = c;
+        howManyPencils =0;
+    }
+    public Box(){  // default constructor
+        type = "plain";
+        height = 1;
+        length = 1;
+        width = 1;
+        color = "white";
+        howManyPencils = 0;
+    }
+    public void addPencils(int x){
+        howManyPencils += x;
+    }
+    public void addPencils(){
+        howManyPencils++;
+    }
+    public void paint(String whatColor){
+        color = whatColor;
+    }
+    public String getColor(){
+        return color;
+    }
+    public int volume(){
+        return length*width*height;
+    }
 
-		numberOnions = numberOnions + num;
-	}
-	
-	public int howManyOnions() {	// accessor method
+    public String toString(){
+        String s = "A box that is " + color + " size " + length +" "+width + " " +height;
+        s += " that has " + howManyPencils + " pencils.";
+        return s;
+    }
 
-		return numberOnions;
-	}
-	
-	public double getHeight() {
 
-		return height;
-	}
-	public void changeHeight(double h) {
+    public static void main(String[] args){
+        Box b1 = new Box();
+        Box b2 = new Box("shoe", 8,6,3,"purple");
+        System.out.println(b1.getColor()+ " " +b1.volume());
+        System.out.println(b2.getColor()+ " " + b2.volume());
 
-		height = h;
-	}
-	
-	public String toString() {
-		String s = "a box with length " + length +" width " + width +
-				" height " + height + " with "+ numberOnions +" onions.";
-		return s;
-	}
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		Box box1 = new Box(4.5,3.0,2.5,"orange",true);  // create an instance of Box
-		Box box2 = new Box();							// create another instance of Box
-		box1.addOnions(5);			// call a method with first box
-		box1.addOnions(6);
-		int x = box1.howManyOnions();
-		System.out.println("Box 1 has " + x + " onions.");
-		System.out.println(box1);		// print the box - this calls toString()
-		System.out.println("volume of box1 is " + box1.volume());
-		// change the height
-		box1.changeHeight(10.0);
-		double v = box1.volume();
-		System.out.println("new volume of box1 is " + v);
-		// call a method with second box
-		System.out.println("the volume of our default box is "+ box2.volume());
-
-	}
-
+        System.out.println(b2);
+    }
 }
-/*
-constructing a box
-default constructor
-Box 1 has 11 onions.
-a box with length 2.5 width 3.0 height 4.5 with 11 onions.
-volume of box1 is 33.75
-new volume of box1 is 75.0
-the volume of our default box is 1.0
-*/
--- src/YummyFood.java
+++ src/YummyFood.java
@@ -28,5 +28,5 @@ public class YummyFood {
         System.out.println("cream");
     }
 
-    
+
 }
